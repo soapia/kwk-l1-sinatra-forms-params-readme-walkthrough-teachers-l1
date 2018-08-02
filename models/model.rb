9d284048199be75e00b01701575b2ac0f3775402
sofia ongele <sofiaongele@gmail.com>
2018-08-02 18:00:51 +0000

    Done.

---
+++ models/model.rb
@@ -0,0 +1,11 @@
+def get_spooked(input)
+  if input == "one"
+    "you're safe :)"
+    elsif input == "two"
+    "boo! you just got spooked"
+    elsif input == "three"
+    "you're safe :)"
+  else
+    "girl you gotta put some input :("
+end
+end
